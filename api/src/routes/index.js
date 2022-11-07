00a9848e2cc19ef4a65dcb9fbf0276a19b31b6ca
Victor A. Duran B <vicduran3d@hotmail.com>
2022-11-07 17:01:26 -0500

    4 rutas ok

--- api/src/routes/index.js
+++ api/src/routes/index.js
@@ -1,4 +1,6 @@
 const { Router } = require('express');
+const  postActivity  = require('../controller/activity.controller.js');
+const { getCountriesName, getCountriesId, getCountries } = require('../controller/countries.controller.js');
 // Importar todos los routers;
 // Ejemplo: const authRouter = require('./auth.js');
 
@@ -7,6 +9,14 @@ const router = Router();
 
 // Configurar los routers
 // Ejemplo: router.use('/auth', authRouter);
+
+router.get('/countries', getCountries);
+
+router.get('/countries/:idPais', getCountriesId);
+
+router.get('/countries', getCountriesName);
+
+router.post('/activities', postActivity);
 
 
 module.exports = router;
