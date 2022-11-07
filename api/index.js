8af84d74235526f137f56dc081ee0ee37139f13f
Victor A. Duran B <vicduran3d@hotmail.com>
2022-11-07 16:53:43 -0500

    se agrego data api a DB

--- api/index.js
+++ api/index.js
@@ -22,11 +22,23 @@
  //           
 
 const server = require('./src/app.js');
-const { conn } = require('./src/db.js');
+const { conn, Country } = require('./src/db.js');
+const getCountriesPreCarga = require('./src/helpers/precargaCountriesDb.js');
+
 
 // Syncing all the models at once.
 conn.sync({ force: true }).then(() => {
-  server.listen(3001, () => {
+  server.listen(3001, async() => {
+    
+    const allCountries = Country.findAll();
+      // se consulta el modelo Country y se pasa por un condicional 
+      // si no existe info ,se trae la data de la api Contries 
+      // mendiante la funcion getCountriesPreCarga() a la DB.
+    if(!allCountries.length){ 
+      const infoApi = await getCountriesPreCarga()
+      await Country.bulkCreate( infoApi )
+       console.log('Data infoApi upload OK')
+    }
     console.log('%s listening at 3001'); // eslint-disable-line no-console
-  });
+  }); 
 });
